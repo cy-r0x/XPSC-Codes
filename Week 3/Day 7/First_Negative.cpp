aa4a3e013283bff8ff08194b7d416914b9649081
cy-r0x <payel15-5106@diu.edu.bd>
2024-11-19 23:41:47 +0600

    added Week 3 Day 7

---
+++ Week 3/Day 7/First_Negative.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
+        // write code here
+        queue<int> q;
+        int l=0,r=0;
+        vector<int> ans;
+        while(r<arr.size()){
+            if(arr[r]<0){
+                q.push(arr[r]);
+            }
+            if(r-l+1==k){
+                if(q.size()) ans.push_back(q.front());
+                else ans.push_back(0);
+                if(arr[l]<0){
+                    q.pop();
+                }
+                l++;
+                r++;
+            }
+            else r++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.FirstNegativeInteger(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
